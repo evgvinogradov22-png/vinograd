3af5afe4942d1482616a88711e304b06060a8730
evgvinogradov22-png <evgvinogradov22@gmail.com>
2026-03-04 10:05:10 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { api, createWS } from "./api";
 import LoginScreen from "./LoginScreen";
 
